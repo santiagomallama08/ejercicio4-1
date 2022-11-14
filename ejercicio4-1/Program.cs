a62078b7fad08b778d3219ea33254dff40bc44b1
SANTIAGO <SANTIAGO@LAPTOP-REHH1RB2>
2022-11-13 21:35:47 -0600

    Agregar archivos de proyecto.

---
+++ ejercicio4-1/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Ejercicios
+{
+    public class program
+    {
+        public static void Main()
+        {
+            int i;
+            int n, sum;
+            int suma;
+            i = 1;
+            suma = 0;
+            while (i <= 10)
+            {
+                Console.WriteLine("Ingresa un numero: ");
+                n = Convert.ToInt32(Console.ReadLine());
+                suma = suma + n;
+                i++;
+            }
+            Console.WriteLine($"La suma de los 10 numeros es {suma}");
+            return;
+        }
+    }
+}
